3fbb9dbe9173dccc65e47c2a4ae203d1984e8380
茶心 <2953015964@qq.com>
2023-02-24 12:35:33 +0800

    创建路由守卫，设置权限访问

--- src/permission.js
+++ src/permission.js
@@ -1,64 +1,29 @@
-// import router from './router'
-// import store from './store'
-// import { Message } from 'element-ui'
-// import NProgress from 'nprogress' // progress bar
-// import 'nprogress/nprogress.css' // progress bar style
-// import { getToken } from '@/utils/auth' // get token from cookie
-// import getPageTitle from '@/utils/get-page-title'
-
-// NProgress.configure({ showSpinner: false }) // NProgress Configuration
-
-// const whiteList = ['/login'] // no redirect whitelist
-
-// router.beforeEach(async(to, from, next) => {
-//   // start progress bar
-//   NProgress.start()
-
-//   // set page title
-//   document.title = getPageTitle(to.meta.title)
-
-//   // determine whether the user has logged in
-//   const hasToken = getToken()
-
-//   if (hasToken) {
-//     if (to.path === '/login') {
-//       // if is logged in, redirect to the home page
-//       next({ path: '/' })
-//       NProgress.done()
-//     } else {
-//       const hasGetUserInfo = store.getters.name
-//       if (hasGetUserInfo) {
-//         next()
-//       } else {
-//         try {
-//           // get user info
-//           await store.dispatch('user/getInfo')
-
-//           next()
-//         } catch (error) {
-//           // remove token and go to login page to re-login
-//           await store.dispatch('user/resetToken')
-//           Message.error(error || 'Has Error')
-//           next(`/login?redirect=${to.path}`)
-//           NProgress.done()
-//         }
-//       }
-//     }
-//   } else {
-//     /* has no token*/
-
-//     if (whiteList.indexOf(to.path) !== -1) {
-//       // in the free login whitelist, go directly
-//       next()
-//     } else {
-//       // other pages that do not have permission to access are redirected to the login page.
-//       next(`/login?redirect=${to.path}`)
-//       NProgress.done()
-//     }
-//   }
-// })
-
-// router.afterEach(() => {
-//   // finish progress bar
-//   NProgress.done()
-// })
+// 路由守卫
+import router from './router'
+import store from './store'
+import nProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+// 添加路由守卫：判断有无token，有的情况下判断是否跳转登录页，如果是则免登录，直接跳回首页
+// 如果没有，则判断是否跳转的是白名单，也就是说跳转是不需要权限的页面，如果是则执行，如果是需要权限的页面，则先登录
+const paths = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+  nProgress.start()
+  if (store.getters.token) {
+    // 有token的情况
+    if (to.path === '/login') {
+      next('/')// 跳转到首页
+    } else {
+      next()
+    }
+  } else {
+    if (paths.includes(to.path)) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+  nProgress.done()
+})
+router.afterEach(() => {
+  nProgress.done()
+})
